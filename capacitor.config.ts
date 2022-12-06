cdcf1aef3e34df58ad68157ae94e52bc1591826c
João <17042005adonel@gmail.com>
2022-12-06 14:25:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'atividade_complementar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
